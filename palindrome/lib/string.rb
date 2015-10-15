34698366ab4b779bb1267ad2691c337a6f010164
Wilson Okibe <wilson@dealdey.com>
2015-10-15 18:14:06 +0100

    remove method argument

--- palindrome/lib/string.rb
+++ palindrome/lib/string.rb
@@ -1,10 +1,10 @@
 class String
   def result
-    is_palindrome?(self) ? puts("#{self} is a palindrome") : puts("#{self} is not a palidrome")
+    self.is_palindrome? ? puts("#{self} is a palindrome") : puts("#{self} is not a palidrome")
     puts
   end
 
-  def is_palindrome?(string)
-    string == string.reverse
+  def is_palindrome?
+    self == self.reverse
   end
 end
