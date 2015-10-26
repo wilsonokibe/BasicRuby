2b3fcd95d749278cab96ff55e310c427896d7003
Wilson Okibe <wilson@dealdey.com>
2015-10-26 10:14:06 +0100

    implement exception within class definition

--- factorial_exception_handling/bin/main.rb
+++ factorial_exception_handling/bin/main.rb
@@ -1,13 +1,6 @@
 require_relative "../lib/fixnum.rb"
 
-begin
-  puts "Specify a number and I will calculate it's factorial"
-  number = Integer(gets.chomp)
-  if number < 0
-    puts "Invalid input for factorial"
-  else
-    p number.calculate_factorial
-  end
-rescue Exception
-  STDERR.puts "Invalid input error: #{$!}"
-end
+puts "Specify a number and I will calculate it's factorial"
+number = Integer(gets.chomp)
+
+p number.calculate_factorial
--- factorial_exception_handling/lib/fixnum.rb
+++ factorial_exception_handling/lib/fixnum.rb
@@ -1,9 +1,13 @@
 class Fixnum
   def calculate_factorial
-    if self < 0
-      puts "Invalid input for factorial"
-    else
-      return (2..self).inject(1) { |a, b| a * b }
+    begin
+      if self < 0
+        raise "Invalid input for factorial" 
+      else
+        return (2..self).inject(1) { |a, b| a * b }
+      end
+    rescue Exception
+      STDERR.puts "Invalid input error: #{$!}"
     end
   end
 end
