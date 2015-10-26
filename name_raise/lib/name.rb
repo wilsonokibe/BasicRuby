4e92e2238cc220e7d330b05b3c1b352a53f8d283
Wilson Okibe <wilson@dealdey.com>
2015-10-26 14:58:58 +0100

    optimize code to eradicate new object creation by NameException

--- name_raise/lib/name.rb
+++ name_raise/lib/name.rb
@@ -4,11 +4,11 @@ class Name
   attr_reader :firstname, :lastname
 
   def initialize(firstname, lastname)
-    raise NameException.new, "Firstname cannot be blank" if firstname.empty?
+    raise NameException, "Firstname cannot be blank" if firstname.empty?
       
-    raise NameException.new, "Lastname cannot be blank" if lastname.empty?
+    raise NameException, "Lastname cannot be blank" if lastname.empty?
       
-    raise NameException.new, "Firstname must start with uppercase letter" if !firstname.match(/[A-Z].*/)
+    raise NameException, "Firstname must start with uppercase letter" if !firstname.match(/[A-Z].*/)
 
     @firstname = firstname
     @lastname  = lastname
