9f54376fd3354f2dc53825e15ed0df405edfe18c
Wilson Okibe <wilson@dealdey.com>
2015-10-26 15:25:39 +0100

    remove return as ruby always returns sth

--- factorial_exception_handling/lib/fixnum.rb
+++ factorial_exception_handling/lib/fixnum.rb
@@ -4,7 +4,7 @@ class Fixnum
       if self < 0
         raise "Invalid input for factorial" 
       else
-        return (2..self).inject(1) { |a, b| a * b }
+        (2..self).inject(1) { |a, b| a * b }
       end
     rescue Exception
       STDERR.puts "Invalid input error: #{$!}"
